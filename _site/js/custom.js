4e68c54ad386310739658cc676538abb2e34f989
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2013-09-18 03:11:01 +0200

    Finished title page

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -1,1 +1,6 @@
-//custom js goes here
+
+
+// Create a team view
+$('.team').icebearTeam({
+        datasource : '/proxy.php?url=https://raw.github.com/MyRealityCoding/galacticum/master/res/meta.json'
+});
